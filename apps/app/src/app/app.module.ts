bdd1073477a337b0d85d1fe973fa35d69acbcab4
Denis Akira Ise Washio <denis@readyon.ai>
2024-03-11 16:11:39 -0300

    WIP

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'Akira1071528',
+      username: 'postgres',
+      autoLoadEntities: true,
+      database: 'test',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
